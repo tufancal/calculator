b80e0709f18b5c1d62af776bfb770e0be6e1e676
twofun2fun <tufan.calisir@outlook.de>
2022-03-20 18:55:00 +0100

    Added: basic functionality

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 "use strict";
+
+// functions for basic mathematic operations
+const add = function (a, b) {
+  return a + b;
+};
+
+const subtract = function (a, b) {
+  return a - b;
+};
+
+const multiply = function (a, b) {
+  return a * b;
+};
+
+const divide = function (a, b) {
+  return a / b;
+};
+
+// function to take an operator and call one of the functions above
+const operate = function (operator, a, b) {
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      return divide(a, b);
+  }
+};
